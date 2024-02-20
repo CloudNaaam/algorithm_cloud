e6df6f5a8b0690294ec41098631c3e8a56963f9b
CloudNaaam <130891758+CloudNaaam@users.noreply.github.com>
2024-02-20 13:52:25 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    cnt = 0
+    
+    for i in range(1,n+2):
+        answer = answer + 1 if cnt >= 3 else answer
+        cnt = 0
+        for j in range(1,i+2):
+            cnt = cnt + 1 if i % j == 0 else cnt
+    
+    return answer
