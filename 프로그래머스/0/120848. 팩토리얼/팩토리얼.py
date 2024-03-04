3d424481b52ce3d16e3d917187f0c99ef600fc9d
CloudNaaam <130891758+CloudNaaam@users.noreply.github.com>
2024-03-04 15:39:03 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 1
+    
+    for i in range(1,n+1):
+        answer = answer * i
+        
+        if answer > n:
+            return i-1
+
+    return answer
+        
